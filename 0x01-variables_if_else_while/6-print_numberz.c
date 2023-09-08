c47f153fe8cf0ea44916fcd50181389842de0410
Marwa <imarwa88@icloud.com>
2023-09-08 12:49:13 -0700

    ADD

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int m;
+	
+	for (m = '0'; m <= '9'; m++)
+	{
+	     putchar(m);
+        }
+
+        putchar('\n');
+
+        return (0);
+
+}	

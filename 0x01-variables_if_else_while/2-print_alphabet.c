32d8edf8479915412ddf429c02dd459557608e3f
Marwa <imarwa88@icloud.com>
2023-09-08 12:01:19 -0700

    Alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,13 @@
  *
  * Return: Always (Success)
  */
-
-int main(void) {
+int main(void)
+{
 	char c;
-	for (c = 'a'; c <= 'z'; c++);
-	     putchar(c);
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
 	putchar('\n');
 	return (0);
-}
+}}

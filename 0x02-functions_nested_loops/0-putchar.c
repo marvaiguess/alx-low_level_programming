7dd64207aee2f81b8fc899834e9ba71e437e0eb4
Marwa <imarwa88@icloud.com>
2023-09-12 03:24:56 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void) 
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
 {
 	printf("_putchar\n");
 	return (0);

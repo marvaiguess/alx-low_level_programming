421f3e900dd9b61a122574e32f8f2f4eefe4e2aa
Marwa <imarwa88@icloud.com>
2023-09-12 06:20:18 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,25 +1,18 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
- *
- *
  * print_alphabet - wirte the the lower-case alphabet on a line.
+ *
+ *
+ *
  */
 void print_alphabet(void)
 {
 	char c;
-	
-	for (c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a' ; c <= 'z' ; c++)
 	{
 		_putchar(c);
 	}
-
-
 	_putchar('\n');
-
-
 }
 
